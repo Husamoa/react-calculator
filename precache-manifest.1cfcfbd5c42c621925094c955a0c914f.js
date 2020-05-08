55f60e94d3de0511c6c9c57833d36e59d90349b2
Husamoa <pawebial@gmail.com>
2020-05-08 19:19:14 +0200

    Updates

---
+++ precache-manifest.1cfcfbd5c42c621925094c955a0c914f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfeaf193dfc219fa12f6dce0b51b8288",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "01d02a920a30eb5fb73c",
+    "url": "/react-calculator/static/css/main.48aaf3c9.chunk.css"
+  },
+  {
+    "revision": "6a53f2077acbfa7228fb",
+    "url": "/react-calculator/static/js/2.02b5f068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.02b5f068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01d02a920a30eb5fb73c",
+    "url": "/react-calculator/static/js/main.9368881c.chunk.js"
+  },
+  {
+    "revision": "f063648edece7b553193",
+    "url": "/react-calculator/static/js/runtime-main.5bb5b4a0.js"
+  }
+]);
